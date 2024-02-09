8e9b6abea38b2258b8cf5af6dcc8b4a3095b4a1a
Oleksiy Grebenyuk <Aleksey191919@gmail.com>
2024-02-10 00:06:26 +0200

    corrected mistakes

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-function userInfo () {
+
     const name = prompt("Enter your name please:");
     const surname = prompt("Enter your surname please:");
     const age = prompt("Enter your age please:");
     if (name && surname && age) {
-        const result = alert(`Hello ${name} ${surname} you are ${age} years old`);
+        alert(`Hello ${name} ${surname} you are ${age} years old`);
     }
-}
-userInfo();
-
 
 /********************************************************************************/
 
-function numberOf5 () {
     const num = prompt("Введіть будь-яке число з 5 цифр:");
     let stringOfNumbers = '';
-    let result;
     if (num.length === 5 && !isNaN(num)) {
         let line = num.toString();
         for (let i = 0; i < line.length; i++) {
             stringOfNumbers += line[i] + " ";
         }
-        result = alert(`Ви отримали строку: ${stringOfNumbers}`);
+        alert(`Ви отримали строку: ${stringOfNumbers}`);
     }
 
     else {
-        result = alert("Ви ввели не правильні дані");
+        alert("Ви ввели не правильні дані");
     }
-}
-
-numberOf5();
